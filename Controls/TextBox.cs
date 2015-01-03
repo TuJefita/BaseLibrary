8837db019814aa21f32e921176aa9ecf4431cb1f
Hector Soto Garcia <h-gods-x@hotmail.com>
2015-01-02 19:27:05 -0700

    TextBox
    TextBox Class

---
+++ Controls/TextBox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framweork.Input;
+
+namespace BaseLibrary.Controls
+{
+    class TextBox
+    {
+
+    }
+}
